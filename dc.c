5283a1a19866dcc58415a8926ab9584eb6fa555a
svetlana <ssvetlana311223@gmail.com>
2017-02-26 18:03:49 +0700

    Считываем размер вклада и проверяем на корректность

--- dc.c
+++ dc.c
@@ -1,1 +1,19 @@
 #include <iostream>
+using namespace std;
+int getvklad ()
+{
+ int p = 1, x;
+ while (p)
+    if (x < 10)
+     { 
+       p = 1;
+       cout << "Минимальная сумма вклада должна быть 10 т.р.";
+     }
+    else 
+     { 
+       p = 0;
+       cout << "Введите сумму вклада: ";
+       cin >> x;
+     }
+    return x;
+}
